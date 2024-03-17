bdbc4bebd63010b1b8bf5e07b760afee8d23e133
Maryam <159541999+im-Maryam-Mahmood@users.noreply.github.com>
2024-03-17 20:44:48 +0500

    Initial Commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const slides = document.querySelectorAll(".slide")
+var counter = 0;
+slides.forEach(
+    (slide, index) => {
+        slide.style.left = `${index * 100}%`
+    }
+)
+
+const goPrevious = () => {
+    counter--
+    slideImage()
+}
+
+const goNext = () => {
+    counter++
+    slideImage()
+}
+
+const slideImage = () => {
+    slides.forEach(
+        (slide) => {
+            slide.style.transform = `translateX(-${counter * 100}%)`
+        }
+    )
+}
